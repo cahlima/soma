e7b4624d5f54c27c7e7a9d41c39211916352ad9f
AcaciaLima <caciabad@gmail.com>
2023-08-16 17:10:12 -0300

    first commit

---
+++ src/app/soma/soma.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SomaComponent } from './soma';
+
+
+
+@NgModule({
+  declarations: [
+    SomaComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+export: [
+  SomaComponent
+]
+})
+export class SomaModule { }
